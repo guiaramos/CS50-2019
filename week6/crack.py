1624fb88fc64ccccc8c5581dab0a7ea0984c592c
Guilherme <gui_aramos@outlook.com>
2019-07-18 16:45:49 +0900

    ADD: crack.py - final version

---
+++ week6/crack.py
@@ -0,0 +1,25 @@
+# Modules
+import sys
+import crypt
+
+#  Validation for input
+if len(sys.argv) != 2:
+    print("Usage: crack <hash>")
+    sys.exit(1)
+
+# Variables
+hash_ = sys.argv[1]
+salt = ahash[0:2]
+letters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# Start script
+for fifth in letters:
+    for fourth in letters:
+        for third in letters:
+            for second in letters:
+                for first in letters[1:]:
+                    candidate = f"{first}{second}{third}{fourth}{fifth}".strip()
+
+                    if crypt.crypt(candidate, salt) == hash_:
+                        print(candidate)
+                        sys.exit(0)
